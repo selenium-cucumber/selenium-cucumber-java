639bf494c0cf6af653c7ff9e98a5d7f707be41fd
sameer49 <sameersawant222@gmail.com>
2017-09-25 14:18:07 +0530

    commented local driver path

--- src/main/java/env/DriverUtil.java
+++ src/main/java/env/DriverUtil.java
@@ -1,41 +1,43 @@
 package env;
 
-import java.util.Collections;
-import org.openqa.selenium.*;
-import org.openqa.selenium.chrome.*;
-import org.openqa.selenium.phantomjs.PhantomJSDriver;
-import org.openqa.selenium.remote.DesiredCapabilities;
-import org.openqa.selenium.support.ui.*;
-
 import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchSessionException;
+import org.openqa.selenium.SessionNotCreatedException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.phantomjs.PhantomJSDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.ErrorHandler;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 /**
  * Created by tom on 24/02/17.
  */
 public class DriverUtil {
     public static long DEFAULT_WAIT = 20;
-    private static int DEFAULT_WINDOW_WIDTH = 1920;
-    private static int DEFAULT_WINDOW_HEIGHT = 1080;
-	protected static WebDriver driver;
+    protected static WebDriver driver;
 
     public static WebDriver getDefaultDriver() {
 		if (driver != null) {
 			return driver;
 		}
-        System.setProperty("webdriver.chrome.driver", "./chromedriver");
-        System.setProperty("webdriver.gecko.driver", "./geckodriver");
-        DesiredCapabilities capabilities = null; //DesiredCapabilities.phantomjs();
+        //System.setProperty("webdriver.chrome.driver", "webdrivers/chromedriver.exe");
+        //System.setProperty("webdriver.gecko.driver", "./geckodriver");
+        DesiredCapabilities capabilities = null;
 		capabilities = DesiredCapabilities.firefox();
         capabilities.setJavascriptEnabled(true);
         capabilities.setCapability("takesScreenshot", true);
         driver = chooseDriver(capabilities);
         driver.manage().timeouts().setScriptTimeout(DEFAULT_WAIT,
                 TimeUnit.SECONDS);
-        driver.manage().window().setSize(new Dimension(DEFAULT_WINDOW_WIDTH,
-                DEFAULT_WINDOW_HEIGHT));
+        driver.manage().window().maximize();
         return driver;
     }
 
@@ -47,35 +49,24 @@ public class DriverUtil {
      * @return
      */
     private static WebDriver chooseDriver(DesiredCapabilities capabilities) {
-		String preferredDriver = System.getProperty("WebDriver", "Firefox");
+		String preferredDriver = System.getProperty("browser", "Firefox");
 		boolean headless = System.getProperty("Headless", "true").equals("true");
 		
-		switch (preferredDriver) {
-			case "Chrome":
-				/*
-				ChromeOptions options = new ChromeOptions();
-				options.addArguments("--headless", "--disable-gpu", "--disable-extensions", "--remote-debugging-port=9222");
-				//options.addArguments("--disable-gpu", "--disable-extensions");
-				//options.setBinary("/usr/bin/chromium-browser");
-				options.setBinary("/usr/bin/google-chrome");
-				capabilities.setCapability(ChromeOptions.CAPABILITY, options);
-				ChromeDriver driver = new ChromeDriver(capabilities);
-				*/
-				
-				
-				// /usr/bin/google-chrome
+		switch (preferredDriver.toLowerCase()) {
+			case "chrome":
 				final ChromeOptions chromeOptions = new ChromeOptions();
-				chromeOptions.setBinary("/usr/bin/chromium-browser");
 				if (headless) {
 					chromeOptions.addArguments("--headless");
 				}
 				capabilities.setCapability(ChromeOptions.CAPABILITY, chromeOptions);
-				ChromeDriver driver = new ChromeDriver(capabilities);
+				System.out.println("********************* before driver created");
+				ChromeDriver driver = new ChromeDriver();
+				System.out.println("********************* after driver created");
 				ErrorHandler handler = new ErrorHandler();
 				handler.setIncludeServerErrors(false);
 				driver.setErrorHandler(handler);
 				return driver;
-			case "PhantomJS":
+			case "phantomjs":
 				return new PhantomJSDriver(capabilities);
 			default:
 				//return new PhantomJSDriver(capabilities);
@@ -85,7 +76,7 @@ public class DriverUtil {
 					options.addArguments("-headless", "-safe-mode");
 				}
 				capabilities.setCapability(FirefoxOptions.FIREFOX_OPTIONS, options);
-				final FirefoxDriver firefox = new FirefoxDriver(capabilities);
+				final FirefoxDriver firefox = new FirefoxDriver();
 				return firefox;
 		}
     }
