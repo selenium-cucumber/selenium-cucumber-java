f513008a9af5e23f1bfb0fbe492b766f54917a80
Barath Subramaniam <barath.subramaniam@macys.com>
2018-04-14 19:04:30 -0700

    new updates

--- downloadDriver.bash
+++ downloadDriver.bash
@@ -1,4 +1,7 @@
 #!/bin/bash
-wget https://github.com/mozilla/geckodriver/releases/download/v0.18.0/geckodriver-v0.18.0-linux64.tar.gz
-tar xvfz geckodriver-v0.18.0-linux64.tar.gz
-rm geckodriver-v0.18.0-linux64.tar.gz
+#wget https://github.com/mozilla/geckodriver/releases/download/v0.18.0/geckodriver-v0.18.0-linux64.tar.gz
+#tar xvfz geckodriver-v0.18.0-linux64.tar.gz
+#rm geckodriver-v0.18.0-linux64.tar.gz
+wget https://github.com/mozilla/geckodriver/releases/download/v0.20.1/geckodriver-v0.20.1-macos.tar.gz
+tar xvfz geckodriver-v0.20.1-macos.tar.gz
+rm geckodriver-v0.20.1-macos.tar.gz
